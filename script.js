ec9d724429ed846d04673d5e4e48c95f454389ed
Dev7896 <barmandev402@gmail.com>
2024-12-12 22:36:51 +0530

    changes in javascript

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 // getting all the elements here
 const nameElement = document.querySelector(".name");
 
-document.addEventListener("DOMContentLoaded", () => {
-  let name = localStorage.getItem("name");
-  if (name == null || name == undefined || name == "") {
-    null;
-  } else {
-    nameElement.innerText = name;
+const allLoadingFunctions = {
+  getNameElement: function () {
+    let name = localStorage.getItem("name");
+    if (name == null || name == undefined || name == "") {
+      null;
+    } else {
+      nameElement.innerText = name;
+    }
+  },
+  settingEmpty : function(){
+    const mainElement = document.querySelector('.main-section-two').innerHTML ;
+    console.log(mainElement) ;
+    if(mainElement == '' || mainElement == null || mainElement == undefined){
+        mainElement.innerHTMl = '<h2 class="copy">your task list is empty</h2>' ;
+    }
   }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    // console.log(this)
+    allLoadingFunctions.getNameElement() ;
+    allLoadingFunctions.settingEmpty() ;
+
+
 });
 
 nameElement.addEventListener("click", () => {
